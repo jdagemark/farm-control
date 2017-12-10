e1792a8e9e53579add50ed7034bd4cc39d6c4105
Johannes Dagemark <johannes@dagemark.com>
2017-12-10 07:52:03 +0100

    Major cleanup of the spray script.
    - support for arguments
    - error handling
    - logging using logger
    
    Signed-off-by: Johannes Dagemark <johannes@dagemark.com>

--- spray.sh
+++ spray.sh
@@ -1,27 +1,113 @@
 #!/bin/bash
 
-solenoid="11"
-duration="4"
+# default values
+SPRAY="11"
+DURATION="5"
 
+function echo_log {
+	echo "$1" | logger
+}
 
-logfile=/var/log/indoor-farm.log
+# get input
+print_usage() {
+        echo "Usage: spray.sh --spray-pin 11 --duration 5"
+        echo ""
+        echo "Options:"
+        echo " -h, --help"
+        echo "    Print detailed help screen"
+	echo " -p, --spray-pin"
+	echo "    Which pin that controls the spray."
+        echo " -d, --duration"
+        echo "    Duration the spray should run in seconds."
+        echo ""
+        echo "Example:"
+        echo "spray.sh --spray-pin 11 --duration 5"
+        echo ""
+}
 
-date=`date "+%F %R:%S"`
-echo "$date Spray starting" >> $logfile
+# Make sure the correct number of command line
+# arguments have been supplied
 
-# initialize pins
-echo "$solenoid" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio$solenoid/direction
-solenoidstate="/sys/class/gpio/gpio$solenoid/value"
-echo "0" > $solenoidstate
+if [ $# -lt 1 ]; then
+        print_usage
+        exit 3
+fi
 
-# toggle the solenoid
-echo "1" > $solenoidstate
-sleep $duration
-echo "0" > $solenoidstate
+# read input variables
+while test -n "$1"; do
+    case "$1" in
+        --help)
+            print_usage
+            exit 0
+            ;;
+        -h)
+            print_usage
+            exit 0
+            ;;
+        --spray-pin)
+            SPRAY=$2
+            shift
+            ;;
+        -p)
+            SPRAY=$2
+            shift
+            ;;
+        --duration)
+            DURATION=$2
+            shift
+            ;;
+        -d)
+            DURATION=$2
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            print_usage
+            exit 3
+            ;;
+    esac
+    shift
+done
 
-# deinitialize
-echo "$solenoid" > /sys/class/gpio/unexport
+echo_log "Spray starting"
 
-date=`date "+%F %R:%S"`
-echo "$date Spray finished" >> $logfile
+# function for initializing the pins
+function spray_initialize {
+	echo "$SPRAY" > /sys/class/gpio/export
+	echo "out" > /sys/class/gpio/gpio$SPRAY/direction
+	echo "0" > /sys/class/gpio/gpio$SPRAY/value
+}
+
+# function for deinitialize the pins
+function spray_deinitialize {
+	echo "0" > /sys/class/gpio/gpio$SPRAY/value
+        echo "$SPRAY" > /sys/class/gpio/unexport
+}
+
+# start spray
+function spray_start {
+	echo "1" > /sys/class/gpio/gpio$SPRAY/value
+}
+
+# stop spray
+function spray_stop {
+	echo "0" > /sys/class/gpio/gpio$SPRAY/value
+}
+
+# unexpected abort
+function abort {
+	spray_deinitialize
+	echo_log "Spray aborted"
+	exit 255
+}
+
+trap abort SIGINT SIGTERM
+
+# Do the work
+spray_initialize
+spray_start
+sleep $DURATION
+spray_stop
+spray_deinitialize
+
+echo_log "Spray finished"
