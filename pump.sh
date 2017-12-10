de88dc240bcd33208c835d3171d48d6459c46c14
Johannes Dagemark <johannes@dagemark.com>
2017-12-10 07:35:47 +0100

    Major cleanup of the pump script.
    - support for arguments
    - error handling
    - logging using logger
    
    Signed-off-by: Johannes Dagemark <johannes@dagemark.com>

--- pump.sh
+++ pump.sh
@@ -1,28 +1,103 @@
 #!/bin/bash
 
-pump="8"
-duration="180"
+# default values
+PUMP="8"
+DURATION="180"
 
-logfile=/var/log/indoor-farm.log
+function echo_log {
+	echo "$1" | logger
+}
 
-date=`date "+%F %R:%S"`
-echo "$date Pump starting" >> $logfile
+# get input
+print_usage() {
+        echo "Usage: pump.sh --duration 120"
+        echo ""
+        echo "Options:"
+        echo " -h, --help"
+        echo "    Print detailed help screen"
+        echo " -d, --duration"
+        echo "    Duration the pump should run in seconds."
+        echo ""
+        echo "Example:"
+        echo "pump.sh --duration 120"
+        echo ""
+}
 
+# Make sure the correct number of command line
+# arguments have been supplied
 
-# initialize pins
-echo "$pump" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio$pump/direction
-pumpstate="/sys/class/gpio/gpio$pump/value"
-echo "0" > $pumpstate
+if [ $# -lt 1 ]; then
+        print_usage
+        exit 3
+fi
 
-# toggle the pump
-echo "1" > $pumpstate
-sleep $duration
-echo "0" > $pumpstate
+# read input variables
+while test -n "$1"; do
+    case "$1" in
+        --help)
+            print_usage
+            exit 0
+            ;;
+        -h)
+            print_usage
+            exit 0
+            ;;
+        --duration)
+            DURATION=$2
+            shift
+            ;;
+        -d)
+            DURATION=$2
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            print_usage
+            exit 3
+            ;;
+    esac
+    shift
+done
 
-# deinitialize
-echo "$pump" > /sys/class/gpio/unexport
+echo_log "Pump starting"
 
-date=`date "+%F %R:%S"`
-echo "$date Pump finished" >> $logfile
+# function for initializing the pins
+function pump_initialize {
+	echo "$PUMP" > /sys/class/gpio/export
+	echo "out" > /sys/class/gpio/gpio$PUMP/direction
+	echo "0" > /sys/class/gpio/gpio$PUMP/value
+}
 
+# function for deinitialize the pins
+function pump_deinitialize {
+	echo "0" > /sys/class/gpio/gpio$PUMP/value
+        echo "$PUMP" > /sys/class/gpio/unexport
+}
+
+# start pump
+function pump_start {
+	echo "1" > /sys/class/gpio/gpio$PUMP/value
+}
+
+# stop pump
+function pump_stop {
+	echo "0" > /sys/class/gpio/gpio$PUMP/value
+}
+
+# unexpected abort
+function abort {
+	pump_deinitialize
+	echo_log "Pumping aborted"
+	exit 255
+}
+
+trap abort SIGINT SIGTERM
+
+# Do the work
+pump_initialize
+pump_start
+sleep $DURATION
+pump_stop
+pump_deinitialize
+
+echo_log "Pump finished"
